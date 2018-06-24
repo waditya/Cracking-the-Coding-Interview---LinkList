4163ffc7b1bdfcd727c43600b59d432b8dd435c9
Aditya Dhananjay Wagholikar <waditya02@gmail.com>
2018-06-24 01:55:59 -0500

    Update 1.2 Deduplicate Linklist.java

--- 1.2 Deduplicate Linklist.java
+++ 1.2 Deduplicate Linklist.java
@@ -1,1 +1,38 @@
+package BusinessLogic;
+
+public class LinkedListNode {
+	int data = 0;
+	LinkedListNode next= null;
+	public LinkedListNode() {
+		// TODO Auto-generated constructor stub
+			this.data = 0;
+			this.next = null;
+			}
+	
+	public LinkedListNode(int data) {
+		this.data = data;
+		this.next = null;
+	}
+
+	public int getData() {
+		return data;
+	}
+
+	public void setData(int data) {
+		this.data = data;
+	}
+
+	public LinkedListNode getNext() {
+		return next;
+	}
+
+	public void setNext(LinkedListNode node) {
+		this.next = node;
+	}
+	
+	
+	
+	
+
+}
 
