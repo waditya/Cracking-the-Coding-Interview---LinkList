6a4d6adbe51d7a8d9d402e8de4de36b8f03e7973
Aditya Dhananjay Wagholikar <waditya02@gmail.com>
2018-06-24 01:56:16 -0500

    Update 1.2 Deduplicate Linklist.java

--- 1.2 Deduplicate Linklist.java
+++ 1.2 Deduplicate Linklist.java
@@ -36,3 +36,43 @@ public class LinkedListNode {
 
 }
 
+package BusinessLogic;
+
+import java.util.HashSet;
+import java.util.LinkedList;
+
+
+public class MyLinkList {
+	
+	
+	public void deleteDups(LinkedListNode n) {
+		HashSet<Integer> set = new HashSet<Integer>();
+		
+		
+		LinkedListNode previous = null;
+		while(n!= null) {
+			if (set.contains(n.data)){
+				previous.next = n.next;
+				
+			}
+			else {
+				set.add(n.data);
+				previous = n;
+			}
+			n = n.next;
+		}
+		
+		
+	}
+	
+	public void printLinkList(LinkedListNode n) {
+		
+		while(n!=null) {
+			System.out.println("\n The data value for this node is : "+n.data);
+			n = n.next;
+		}
+	}
+
+}
+
+
