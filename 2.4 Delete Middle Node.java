09a2cb11715fe12da253b1676424129803a544b0
Aditya Dhananjay Wagholikar <waditya02@gmail.com>
2018-07-06 17:37:14 -0500

    Update 2.4 Delete Middle Node.java

--- 2.4 Delete Middle Node.java
+++ 2.4 Delete Middle Node.java
@@ -1,1 +1,11 @@
+/*2.4 Delete Middle Node
+Written by -  Aditya Wagholikar */
 
+boolean deleteNode(LinkedListNode node){
+  if(node  == null || node.next == null){
+      return false; 
+  }
+  node.data = node.next.data;
+  node.next = node.next.next;
+  return true;
+}
