af5fa1ff907866e369a6c776d52d58a9a86faf42
Aditya Dhananjay Wagholikar <waditya02@gmail.com>
2018-07-07 16:26:19 -0500

    Update 2.4 Partition by Pivot without stability.java

--- 2.4 Partition by Pivot without stability.java
+++ 2.4 Partition by Pivot without stability.java
@@ -1,3 +1,29 @@
 /* Pass in the had of the linklist and the value to partition around without stability
 Written by - Aditya Wagholikar 
 Date - 7 Jul 2018 */
+
+LinkListNode partition(LinkListNode node, int partition){
+    LinkListNode head = node;
+    LinkListNode tail = node;
+  
+    while(node != null){
+        LinkListNode next = node.next;
+      
+        if (node.data < partition){
+            /* Insert Node at the head*/
+          
+            node.next = head;
+            head = node;
+        }else{
+            /*Insert node at the tail*/
+            tail.next = node;
+            tail = node;
+        }
+        node = next;
+    }
+    tail.next = null;
+  
+    /*The head has changed , so we need to 
+    return it to the user*/
+  return;
+}
