16f399c714b61d60507acfa5048bfe2eebe0f01e
Aditya Dhananjay Wagholikar <waditya02@gmail.com>
2018-07-12 13:56:30 -0500

    Update 2.6.2 Iterative approach using stack.java

--- 2.6.2 Iterative approach using stack.java
+++ 2.6.2 Iterative approach using stack.java
@@ -1,1 +1,3 @@
-/*Written by - Aditya Wagholikar*/
+/* Iterative approach to determne a whether a link list is a palindrome or not
+I have used stack data structure in it
+Written by - Aditya Wagholikar*/
