59d1d48bc658ada40f0fc8bb955a52950ae84142
Aditya Dhananjay Wagholikar <waditya02@gmail.com>
2018-07-12 13:57:04 -0500

    Update 2.6.2 Iterative approach using stack.java

--- 2.6.2 Iterative approach using stack.java
+++ 2.6.2 Iterative approach using stack.java
@@ -1,3 +1,7 @@
 /* Iterative approach to determne a whether a link list is a palindrome or not
 I have used stack data structure in it
 Written by - Aditya Wagholikar*/
+
+boolean isPalindrome(LinkListNode node){
+  return true;
+}
