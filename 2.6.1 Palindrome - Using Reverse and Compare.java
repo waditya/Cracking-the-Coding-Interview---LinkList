b2048db4d2a663df86c458e117863fbdc63071de
Aditya Dhananjay Wagholikar <waditya02@gmail.com>
2018-07-11 19:31:38 -0500

    Update 2.6.1 Palindrome - Using Reverse and Compare.java

--- 2.6.1 Palindrome - Using Reverse and Compare.java
+++ 2.6.1 Palindrome - Using Reverse and Compare.java
@@ -1,6 +1,6 @@
 boolean isPalindrome(LinkedListNode head){
   LinkedListNode reversed = reverseAndClone(LinkedListNode head);
-  return isEqual(head, reversed);
+  return isLinkListEqual(head, reversed);
 }
 
 LinkedListNode reverseAndClone(LinkedListNode node){
@@ -12,4 +12,18 @@ LinkedListNode reverseAndClone(LinkedListNode node){
       node = node.next;
     }
   return head;
+}
+
+boolean isLinkListEqual(LinkedListNode l1, LinkedListNode l2){
+  while(l1 != null && l1 != null){
+    if(l1.value != l2.value){
+      return false;
+    }
+    
+    l1 = l1.next;
+    l2 = l2.next;
+    
+  }
+  //This ensures that link list with unequal lengths are not equal
+  return l1 == null && l2 == null
 }
