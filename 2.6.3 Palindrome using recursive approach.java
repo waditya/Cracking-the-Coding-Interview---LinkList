8f54c2664a0de874deb6f8241ba451e19dcb7e6b
Aditya Dhananjay Wagholikar <waditya02@gmail.com>
2018-07-15 01:32:11 -0500

    Update 2.6.3 Palindrome using recursive approach.java

--- 2.6.3 Palindrome using recursive approach.java
+++ 2.6.3 Palindrome using recursive approach.java
@@ -1,5 +1,5 @@
 /*Palindrome Check using Recursion
-Written By - Aditya Wagholikar*/
+Written By - Aditya Wagholikar. */
 boolean isPalindrome(LinkListNode head){
   int len = lengthOfList(head);
   Result p = isPalindromeRecurse(head, len);
