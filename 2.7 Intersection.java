c05e570618b864ad0d79ae3e3473386cf8963016
Aditya Dhananjay Wagholikar <waditya02@gmail.com>
2018-07-16 18:53:00 -0500

    Update 2.7 Intersection.java

--- 2.7 Intersection.java
+++ 2.7 Intersection.java
@@ -1,2 +1,9 @@
 /* Intersection in a link list - Determine the common node (intersection) by reference
 Written By - Aditya Wagholikar. */
+
+LinkedListNode findIntersection(LinkedListNode list1, LinkedListNode list2){
+  if(list1 == null || list2 == null){
+    //If either of the list is null, return null i.e. there is no intersection
+    return null;
+  }
+}
