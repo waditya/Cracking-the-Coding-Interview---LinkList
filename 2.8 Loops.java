3dff7d1314cc4d421c0f878d5f0a75e1c2e0f5c2
Aditya Dhananjay Wagholikar <waditya02@gmail.com>
2018-07-17 23:14:27 -0500

    Update 2.8 Loops.java

--- 2.8 Loops.java
+++ 2.8 Loops.java
@@ -1,2 +1,10 @@
 /*Loops - Detect loops in linkedlist using fast runner and slow runner approach
 Written by - Aditya Wagholikar*/
+
+LinkedListNode findBeginningNode(LinkedListNode head){
+  LinkedListNode ptrSlow = head;
+  LinkedListNode ptrFast = head;
+  
+  /*Find the meeting point. It should be LOOP_LENGTH - k steps into the linkedlist*/
+}
+
