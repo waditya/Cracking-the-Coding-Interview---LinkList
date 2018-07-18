aac766bb1924de7533a93631fc5c8aaeaffc7176
Aditya Dhananjay Wagholikar <waditya02@gmail.com>
2018-07-17 23:20:53 -0500

    Update 2.8 Loops.java

--- 2.8 Loops.java
+++ 2.8 Loops.java
@@ -6,5 +6,35 @@ LinkedListNode findBeginningNode(LinkedListNode head){
   LinkedListNode ptrFast = head;
   
   /*Find the meeting point. It should be LOOP_LENGTH - k steps into the linkedlist*/
+  
+  while(ptrFast != null && ptrFast.next!= null){
+    ptrSlow = sptrSlow.next;
+    ptrFast = ptrFast.next.next;
+    
+    if(ptrSlow == ptrFast){
+      //This happens either in case of loop  intersection or nodes reaching end of linked list (which do not have loop)
+      break;
+    }
+  }
+  
+  /*Error check - no meeting point and therefore no loop*/
+  
+  if(fast == null || fast.next == null){
+    return null;
+  }
+  
+  /*Move slowPtr to head. Keep fast at meeting point. Each are k steps from the loop start. 
+  If they move at same pace, they must meet at loop start*/
+  
+  slowPtr = head;
+  
+  while(slowPtr != fastPtr){
+    slowPtr = slowPtr.next;
+    fastPtr = fastPtr.next;
+  }
+  
+  /*Both now point to the same starting point of the LOOP*/
+  
+  return fastPtr;
 }
 
